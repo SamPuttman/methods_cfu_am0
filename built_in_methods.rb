9be62a89c433f2795149e53355b25999510f1e0d
Sam Puttman <daputter2@gmail.com>
2023-09-03 15:47:10 -0500

    add code to questions

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -8,19 +8,29 @@
 # The return value is "hello world"
 "Hello World".downcase
 
-
+# The include method is called on the string object "Hello World"
+# Method include? has one job which is to verify that the argument "Hello" within the parameter exists in the String
+# The return value is true
 "Hello World".include?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The end_with? method is utilized to check if the specified string end with the specified parameter
+# The return value is false
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method is called on the string object "Hello World"
+# The end_with? method is utilized to check if the specified string end with the specified parameter
+# The return value is true
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the Integer 12
+# The even? method is utilized to check if the preceding integer is an even number
+# The return value is true
 12.even?
 
-
+# The next method is called on the Integer 18
+# The next method is utilized to add 1 to the preceeding integer
+# The return value is 19
 18.next
 
 
@@ -31,6 +41,7 @@
 # https://ruby-doc.org/core-3.1.0/String.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+
 # EXAMPLE
 # The start_with? method is called on the first_name variable, which stores the string object "Jeff". 
 # The start_with? method returns true if the data in the first_name variable starts with the argument passed in.
@@ -40,6 +51,21 @@ first_name = "Jeff"
 puts first_name.start_with?("J")
 
 
+# The upcase method is called on the cat variable, which stores the string object "Tom"
+# The upcase method is utilized to capitalize all characters in a string.
+# The capitalize method is called on the mouse variable, which stores the string object "JERRY"
+# The capitalize method is utilized to capitalize the first character in a string and downcase the remaining characters.
+# In this example, return value is:
+# => "TOM"
+# => "Jerry"
+
+
+cat = "Tom"
+mouse = "JERRY"
+
+puts cat.upcase 
+puts mouse.capitalize
+
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
@@ -48,9 +74,37 @@ puts first_name.start_with?("J")
 # Include comments above each method call explaining the impact and return value of that method.
 
 
+#The even? method is called on the number_of_cats variable, which stores the integer 5
+#The even? method is used to verify if the assigned integer value is an even number, and returns a boolean value
+#The integer? method is called on the number_of_dogs variable, which stores the integer 2
+#The integer? method is used to verify if the preceding variable is an integer, and returns a boolean value
+#In this example, the return value is:
+# => false
+# => true
+
+number_of_cats = 5
+number_of_dogs = 2
+
+puts number_of_cats.even?
+puts number_of_dogs.integer?
+
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+#The unshift method is called on the array age_of_cats, which stores the integer values [5, 7, 9, 12]
+#The unshift method is utilized to add the parameter value to the beginning of an array. 
+#The reverse method is called on the array age_of_dogs, which stores the integer values [2, 6, 8, 11]
+#The reverse method is utilized to reverse the values in the preceeding array.
+#In this example, the return value is:
+# => [3, 5, 7, 9, 12]
+# => [11, 8, 6, 2]
+
+age_of_cats = [5, 7, 9, 12]
+age_of_dogs = [2, 6, 8, 11]
+
+print age_of_cats.unshift(3)
+print age_of_dogs.reverse
--- final_practice.rb
+++ final_practice.rb
@@ -6,34 +6,92 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
+def greet_a_cat(cat_breed)
+    return "Meowdy, #{cat_breed} cat!"
+end
+
+puts greet_a_cat("Siamese")
+puts greet_a_cat("Calico")
+
 # What is the return value of your method?
+   # The return value is:
+   # => "Meowdy, Siamese cat!"
+   # => "Meowdy, Calico cat!"
 # How many arguments did you pass your method?
+   # 1
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
+def custom_greeting(name)
+    return "Greetings, #{name}!"
+end
+
+puts custom_greeting("Castiel")
+puts custom_greeting("Loki")
+
 # What is the return value of your method?
+   # The return value is:
+   # => Greetings, Castiel!
+   # => Greeting, Loki!
 # How many arguments did you pass your method?
+   # 1
 # What data type was your argument(s)?
+   # String
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
+def greet_person(first_name, middle_name, last_name)
+    full_name = "#{first_name} #{middle_name} #{last_name}"
+    return "Greetings, #{full_name}! You are looking snazzy today!"
+end
+
+puts greet_person("Samuel", "James", "Puttman")
+puts greet_person("Ronald", "MacGodDamn", "Donald")
+
 # What is the return value of your method?
+   # Greetings, Samuel James Puttman! You are looking snazzy today!
+   # Greetings, Ronald MacGodDamn Donald! You are looking snazzy today!
+
 # How many arguments did you pass your method?
+   # 3
 # What data type was your argument(s)?
+   # Strings
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
+def square(integer)
+    result = Math.sqrt(integer)
+    return "The square root of #{integer} is #{result}"
+end
+
+puts square(12)
+puts square(19)
+
 # What is the return value of your method?
+   # => The square root of 12 is 3.4641016151377544
+   # => The square root of 19 is 4.358898943540674
 # How many arguments did you pass your method?
+   # 1
 # What data type was your argument(s)?
+   # integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+def check_stock(number_available, item)
+    if number_available >= 4
+        puts "#{item} is stocked"
+    elsif number_available == 0
+        puts "#{item} - OUT of stock"
+    else number_available < 3
+        puts "#{item} - running LOW"
+    end
+end
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
